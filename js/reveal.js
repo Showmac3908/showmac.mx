798a9ad08bc344484be5dfeb69f6cbb650f10132
Showmac3908 <marketing@showmac.com.mx>
2022-01-14 18:31:15 -0600

    index

---
+++ js/reveal.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal(".imagen1", {delay: 600});
+ScrollReveal().reveal(".imagen2", {delay: 800});
+ScrollReveal().reveal(".imagen3", {delay: 1000});
+ScrollReveal().reveal(".imagen4", {delay: 1200});
+ScrollReveal().reveal(".imagen5", {delay: 1600});
+ScrollReveal().reveal(".button-grid", {delay: 1800});
