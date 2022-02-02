59c8cd59092366ac9b55f6bfbf282a982cb6a099
Showmac3908 <marketing@showmac.com.mx>
2022-02-01 19:28:41 -0600

    Efectos servicios

--- js/reveal.js
+++ js/reveal.js
@@ -4,3 +4,12 @@ ScrollReveal().reveal(".imagen3", {delay: 1000});
 ScrollReveal().reveal(".imagen4", {delay: 1200});
 ScrollReveal().reveal(".imagen5", {delay: 1600});
 ScrollReveal().reveal(".button-grid", {delay: 1800});
+
+
+ScrollReveal().reveal(".evento1", {delay: 400});
+ScrollReveal().reveal(".evento2", {delay: 600});
+ScrollReveal().reveal(".evento3", {delay: 800});
+ScrollReveal().reveal(".evento4", {delay: 1000});
+
+ScrollReveal().reveal(".serv1", {delay: 400});
+ScrollReveal().reveal(".serv2", {delay: 600});
